43bfb0717c71fc65de40c103a86e7fcf09f40347
YazawaNaoki <yazawa.naoki.infratop@gmail.com>
2020-04-07 18:35:28 +0900

    add user_id

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#top'
-  post '/callback' => 'linebot#recieve'
+  post '/callback' => 'linebot#callback'
   namespace 'api' do
     namespace 'v1' do
       resources :messages
