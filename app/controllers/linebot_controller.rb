6854b4737281a4a866cded41afe4660b0cfb9e73
YazawaNaoki <yazawa.naoki.infratop@gmail.com>
2020-04-07 18:40:30 +0900

    fix

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -37,15 +37,15 @@ class LinebotController < ApplicationController
     head :ok
   end
 
-  def recieve
-    body = request.body.read
-    events = client.parse_events_from(body)
-    events.each { |event|
-      userId = event['source']['userId']  #userId取得
-      p 'UserID: ' + userId # UserIdを確認
-    end
-    }
-  end
+  # def recieve
+  #   body = request.body.read
+  #   events = client.parse_events_from(body)
+  #   events.each { |event|
+  #     userId = event['source']['userId']  #userId取得
+  #     p 'UserID: ' + userId # UserIdを確認
+  #   end
+  #   }
+  # end
 
   private
 
